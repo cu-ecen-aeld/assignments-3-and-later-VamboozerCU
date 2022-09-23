e0f6ee5d3603f8b220437db5adadd7460bd3161e
Vamboozer <marshallfolkman@gmail.com>
2022-09-23 12:22:55 -0600

    Stuck on 3-2

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -4,8 +4,10 @@
 
 set -e
 set -u
+export PATH=$PATH:/home/marshall/Programs/install-lnx/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/bin
 
 OUTDIR=/tmp/aeld
+ARCHIVE=~/Documents/ECEA5305_LinuxSystemProgrammingIntroToBuildroot/assign3-2
 KERNEL_REPO=git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
 KERNEL_VERSION=v5.1.10
 BUSYBOX_VERSION=1_33_1
@@ -13,13 +15,23 @@ FINDER_APP_DIR=$(realpath $(dirname $0))
 ARCH=arm64
 GNU_PATH=/home/marshall/Programs/install-lnx/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/bin
 LIB_PATH=/home/marshall/Programs/install-lnx/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu
-CROSS_COMPILE=${GNU_PATH}/aarch64-none-linux-gnu-
+#CROSS_COMPILE=${GNU_PATH}/aarch64-none-linux-gnu-
+CROSS_COMPILE=aarch64-none-linux-gnu-
+FINDER_APP=/home/marshall/git/assignment-1-VamboozerCU/finder-app
 
-if [ -d $GNU_PATH ]
+if [ -d $FINDER_APP ]
 then
     : # NOP
 else
-    echo "GNU_PATH Does NOT Exist"
+    echo "FINDER_APP Does NOT Exist"
+    exit 1
+fi
+
+if [ -d $ARCHIVE ]
+then
+    : # NOP
+else
+    echo "ARCHIVE Does NOT Exist"
     exit 1
 fi
 
@@ -31,22 +43,28 @@ else
 	echo "Using passed directory ${OUTDIR} for output"
 fi
 
+echo "Creating the staging directory for the root filesystem"
+if [ -d ${OUTDIR} ]
+then
+	echo "Deleting output directory at ${OUTDIR} and starting over"
+    sudo rm -rf ${OUTDIR}
+fi
 mkdir -p ${OUTDIR}
 
-cd "$OUTDIR"
-if [ ! -d "${OUTDIR}/linux-stable" ]; then
+cd ${ARCHIVE}
+if [ ! -d "${ARCHIVE}/linux-stable" ]; then
     #Clone only if the repository does not exist.
-	echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
+	echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${ARCHIVE}"
 	git clone ${KERNEL_REPO} --depth 1 --single-branch --branch ${KERNEL_VERSION}
 fi
-if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
+if [ ! -e ${ARCHIVE}/linux-stable/arch/${ARCH}/boot/Image ]; then
     cd linux-stable
     echo "Checking out version ${KERNEL_VERSION}"
     git checkout ${KERNEL_VERSION}
 
-    echo "Apply Ubuntu V22.04 patch"
-    wget https://github.com/torvalds/linux/commit/e33a814e772cdc36436c8c188d8c42d019fda639.diff
-    git apply e33a814e772cdc36436c8c188d8c42d019fda639.diff
+    #echo "Apply Ubuntu V22.04 patch"
+    #wget https://github.com/torvalds/linux/commit/e33a814e772cdc36436c8c188d8c42d019fda639.diff
+    #git apply e33a814e772cdc36436c8c188d8c42d019fda639.diff
 
     echo "Kernel build steps:"
     #echo "Installing dependencies if not already installed..."
@@ -64,14 +82,16 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
 fi
 
 echo "Adding the Image in outdir"
+cp -a ${ARCHIVE}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}/Image
+#rm -f ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image.gz
 
 echo "Creating the staging directory for the root filesystem"
-cd "$OUTDIR"
-if [ -d "${OUTDIR}/rootfs" ]
-then
-	echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
-    sudo rm  -rf ${OUTDIR}/rootfs
-fi
+#cd "$OUTDIR"
+#if [ -d "${OUTDIR}/rootfs" ]
+#then
+#	echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
+#    sudo rm -rf ${OUTDIR}/rootfs
+#fi
 
 echo "Create necessary base directories"
 mkdir -p ${OUTDIR}/rootfs
@@ -80,59 +100,104 @@ mkdir -p bin dev etc home lib proc sbin sys tmp usr var
 mkdir -p usr/bin usr/lib usr/sbin
 mkdir -p var/log
 
-cd "$OUTDIR"
-if [ ! -d "${OUTDIR}/busybox" ]
+cd ${ARCHIVE}
+if [ ! -d "${ARCHIVE}/busybox" ]
 then
-git clone git://busybox.net/busybox.git
+    git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
     echo "Configure busybox"
     make distclean
-    make defconfig
+    #make defconfig
     #sudo make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- defconfig
+    make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- defconfig
 else
     cd busybox
 fi
 
 echo "Make and install busybox"
-#sudo make ARCH=arm CROSS_COMPILE=aarch64-none-linux-gnu- install
-#sudo make ARCH=arm CROSS_COMPILE=arm-unknown-linux-gnueabi- install
-#sudo make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu-
-#sudo make -j4 install
 sudo make -j4 CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=arm64 CROSS_COMPILE=${GNU_PATH}/aarch64-none-linux-gnu- install
 
-echo "Library dependencies"
+echo "Add library dependencies to rootfs"
+# arm-unknown-linux-gnueabi ??
 cd ${OUTDIR}/rootfs
 echo "======Required for program interpreter======="
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
+# [Requesting program interpreter: /lib/ld-linux-aarch64.so.1]
+cp -a ${LIB_PATH}/aarch64-none-linux-gnu/libc/lib/ld-linux-aarch64.so.1 ${OUTDIR}/rootfs/lib/ld-linux-aarch64.so.1
+ls -l ${LIB_PATH}/aarch64-none-linux-gnu/libc/lib/ld-linux-aarch64.so.1
+# ld-linux-aarch64.so.1 -> ../lib64/ld-2.31.so
+cp -a ${LIB_PATH}/aarch64-none-linux-gnu/libc/lib64/ld-2.31.so ${OUTDIR}/rootfs/lib/ld-2.31.so
+
 echo "======Required for Shared library======="
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
-echo "======Required for sysroot======="
-${CROSS_COMPILE}gcc -print-sysroot 
-#ls -l lib/ld-linux-armhf.so.3
-
-echo "Add library dependencies to rootfs"
-cp -a ${LIB_PATH}/aarch64-none-linux-gnu/libc/lib/ld-linux-aarch64.so.1 ${OUTDIR}/rootfs/lib/ld-linux-aarch64.so.1
+# 0x0000000000000001 (NEEDED)             Shared library: [libm.so.6]
+# 0x0000000000000001 (NEEDED)             Shared library: [libresolv.so.2]
+# 0x0000000000000001 (NEEDED)             Shared library: [libc.so.6]
 cp -a ${LIB_PATH}/aarch64-none-linux-gnu/libc/lib64/libm.so.6 ${OUTDIR}/rootfs/lib/libm.so.6
 cp -a ${LIB_PATH}/aarch64-none-linux-gnu/libc/lib64/libresolv.so.2 ${OUTDIR}/rootfs/lib/libresolv.so.2
 cp -a ${LIB_PATH}/aarch64-none-linux-gnu/libc/lib64/libc.so.6 ${OUTDIR}/rootfs/lib/libc.so.6
-
-cp -a ${LIB_PATH}/aarch64-none-linux-gnu/libc/lib64/ld-2.31.so ${OUTDIR}/rootfs/lib/ld-2.31.so
+ls -l ${LIB_PATH}/aarch64-none-linux-gnu/libc/lib64/libm.so.6
+ls -l ${LIB_PATH}/aarch64-none-linux-gnu/libc/lib64/libresolv.so.2
+ls -l ${LIB_PATH}/aarch64-none-linux-gnu/libc/lib64/libc.so.6
+#~/Programs/install-lnx/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libm.so.6 -> libm-2.31.so
+#~/Programs/install-lnx/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libresolv.so.2 -> libresolv-2.31.so
+#~/Programs/install-lnx/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libc.so.6 -> libc-2.31.so
 cp -a ${LIB_PATH}/aarch64-none-linux-gnu/libc/lib64/libm-2.31.so ${OUTDIR}/rootfs/lib/libm-2.31.so
 cp -a ${LIB_PATH}/aarch64-none-linux-gnu/libc/lib64/libresolv-2.31.so ${OUTDIR}/rootfs/lib/libresolv-2.31.so
 cp -a ${LIB_PATH}/aarch64-none-linux-gnu/libc/lib64/libc-2.31.so ${OUTDIR}/rootfs/lib/libc-2.31.so
+
+echo "======Required for sysroot======="
+${CROSS_COMPILE}gcc -print-sysroot 
+#~/Programs/install-lnx/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/bin/../aarch64-none-linux-gnu/libc
+#ls -l lib/ld-linux-aarch64.so.1
 
 echo "Make device nodes"
 sudo mknod -m 666 dev/null c 1 3
 sudo mknod -m 666 dev/console c 5 1
 
-echo "TODO: Clean and build the writer utility"
+echo "Clean and build the writer utility"
+cd ${FINDER_APP}
+make clean
+make all
 
-echo "TODO: Copy the finder related scripts and executables to the /home directory on the target rootfs"
+echo "Copy the finder related scripts and executables to the /home directory on the target rootfs"
+cd
+cp -a ${FINDER_APP} ${OUTDIR}/rootfs/home/finder-app
+cd ${OUTDIR}/rootfs/home/finder-app
+rm manual-linux.sh
+rm -rf conf
+mkdir -p ${OUTDIR}/rootfs/home/finder-app/conf
+cp ${FINDER_APP}/conf/assignment.txt ${OUTDIR}/rootfs/home/finder-app/conf/assignment.txt
+cp ${FINDER_APP}/conf/username.txt ${OUTDIR}/rootfs/home/finder-app/conf/username.txt
 
-exit 1
-echo "TODO: Chown the root directory"
-${OUTDIR}/rootfs
+echo "Copy the autorun-qemu.sh script into the outdir/rootfs/home directory"
+cp ${FINDER_APP}/autorun-qemu.sh ${OUTDIR}/rootfs/home/autorun-qemu.sh
+
+echo "Chown the root directory"
+cd ${OUTDIR}/rootfs
 sudo chown -R root:root *
+# "I would chroot in and rebuild the initramfs, 
+#  while tailing the system log for io error messages."
 
-echo "TODO: Create initramfs.cpio.gz"
+echo "Create initramfs.cpio"
+cd ${OUTDIR}
+rm -f initramfs.cpio.gz
+#find . -depth | cpio -ov > initramfs.cpio
+find rootfs -depth | cpio -o > initramfs.cpio # add -v for debugging
+#find . -name Image | cpio -oA -F initramfs.cpio # add -v for debugging
+
+echo "Create initramfs.cpio.gz"
+#gzip -k initramfs.cpio # -k to keep initramfs.cpio
+gzip initramfs.cpio
+#tar -zcvf initramfs.tar.gz rootfs
+
+if [ -e ${OUTDIR}/initramfs.cpio.gz ]
+#if [ -e ${OUTDIR}/initramfs.tar.gz ]
+then
+    echo "Make SUCCESS!"
+    exit 0
+else
+    echo "FAILED to create initramfs.cpio.gz"
+    exit 1
+fi
--- finder-app/start-qemu-terminal.sh
+++ finder-app/start-qemu-terminal.sh
@@ -4,6 +4,7 @@
 
 set -e
 
+#ARCH=arm64
 OUTDIR=$1
 
 if [ -z "${OUTDIR}" ]; then
@@ -11,8 +12,10 @@ if [ -z "${OUTDIR}" ]; then
     echo "No outdir specified, using ${OUTDIR}"
 fi
 
+#KERNEL_IMAGE=${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image
 KERNEL_IMAGE=${OUTDIR}/Image
 INITRD_IMAGE=${OUTDIR}/initramfs.cpio.gz
+#INITRD_IMAGE=${OUTDIR}/initramfs.tar.gz
 
 if [ ! -e ${KERNEL_IMAGE} ]; then
     echo "Missing kernel image at ${KERNEL_IMAGE}"
@@ -26,7 +29,8 @@ fi
 
 echo "Booting the kernel"
 # See trick at https://superuser.com/a/1412150 to route serial port output to file
-qemu-system-aarch64 -m 256M -M virt -cpu cortex-a53 -nographic -smp 1 -kernel ${KERNEL_IMAGE} \
+qemu-system-aarch64 -m size=8192M -M virt -cpu cortex-a53 -nographic -smp 1 -kernel ${KERNEL_IMAGE} \
         -chardev stdio,id=char0,mux=on,logfile=${OUTDIR}/serial.log,signal=off \
         -serial chardev:char0 -mon chardev=char0\
         -append "rdinit=/bin/sh" -initrd ${INITRD_IMAGE}
+# -m 512 1024 2048 4096 8192
