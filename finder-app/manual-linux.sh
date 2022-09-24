d0315dfd8cd4e4c92f09f5cfb3b9fb8734c1a70b
Vamboozer <marshallfolkman@gmail.com>
2022-09-23 21:10:08 -0600

    Debug Autograder 4

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -9,47 +9,17 @@ export PATH=$PATH:/home/marshall/Programs/install-lnx/gcc-arm-10.2-2020.11-x86_6
 OUTDIR=/tmp/aeld
 FINDER_APP_DIR=$(realpath $(dirname $0))
 #ARCHIVE=~/Documents/ECEA5305_LinuxSystemProgrammingIntroToBuildroot/assign3-2
-ARCHIVE=${FINDER_APP_DIR}/../../../Documents/ECEA5305_LinuxSystemProgrammingIntroToBuildroot/assign3-2
+#ARCHIVE=${FINDER_APP_DIR}/../../../Documents/ECEA5305_LinuxSystemProgrammingIntroToBuildroot/assign3-2
+#GNU_PATH=${FINDER_APP_DIR}/../../../Programs/install-lnx/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/bin
+#LIB_PATH=${FINDER_APP_DIR}/../../../Programs/install-lnx/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu
+
 KERNEL_REPO=git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
 KERNEL_VERSION=v5.1.10
 BUSYBOX_VERSION=1_33_1
+
 ARCH=arm64
-GNU_PATH=${FINDER_APP_DIR}/../../../Programs/install-lnx/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/bin
-LIB_PATH=${FINDER_APP_DIR}/../../../Programs/install-lnx/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu
-#CROSS_COMPILE=${GNU_PATH}/aarch64-none-linux-gnu-
 CROSS_COMPILE=aarch64-none-linux-gnu-
-
-if [ -d $FINDER_APP_DIR ]
-then
-    : # NOP
-else
-    echo "FINDER_APP_DIR Does NOT Exist ${FINDER_APP_DIR}"
-    exit 1
-fi
-
-if [ -d $ARCHIVE ]
-then
-    : # NOP
-else
-    echo "ARCHIVE Does NOT Exist ${ARCHIVE}"
-    exit 1
-fi
-
-if [ -d $GNU_PATH ]
-then
-    : # NOP
-else
-    echo "GNU_PATH Does NOT Exist ${GNU_PATH}"
-    exit 1
-fi
-
-if [ -d $LIB_PATH ]
-then
-    : # NOP
-else
-    echo "LIB_PATH Does NOT Exist ${LIB_PATH}"
-    exit 1
-fi
+export SYSROOT=$(${CROSS_COMPILE}gcc -print-sysroot)
 
 if [ $# -lt 1 ]
 then
@@ -67,13 +37,13 @@ then
 fi
 mkdir -p ${OUTDIR}
 
-cd ${ARCHIVE}
-if [ ! -d "${ARCHIVE}/linux-stable" ]; then
+cd ${OUTDIR}
+if [ ! -d "${OUTDIR}/linux-stable" ]; then
     #Clone only if the repository does not exist.
-	echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${ARCHIVE}"
+	echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
 	git clone ${KERNEL_REPO} --depth 1 --single-branch --branch ${KERNEL_VERSION}
 fi
-if [ ! -e ${ARCHIVE}/linux-stable/arch/${ARCH}/boot/Image ]; then
+if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     cd linux-stable
     echo "Checking out version ${KERNEL_VERSION}"
     git checkout ${KERNEL_VERSION}
@@ -98,7 +68,7 @@ if [ ! -e ${ARCHIVE}/linux-stable/arch/${ARCH}/boot/Image ]; then
 fi
 
 echo "Adding the Image in outdir"
-cp -a ${ARCHIVE}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}/Image
+cp -a ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}/Image
 #rm -f ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image.gz
 
 echo "Creating the staging directory for the root filesystem"
@@ -116,8 +86,8 @@ mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
 mkdir -p usr/bin usr/lib usr/sbin
 mkdir -p var/log
 
-cd ${ARCHIVE}
-if [ ! -d "${ARCHIVE}/busybox" ]
+cd ${OUTDIR}
+if [ ! -d "${OUTDIR}/busybox" ]
 then
     git clone git://busybox.net/busybox.git
     cd busybox
@@ -132,7 +102,7 @@ else
 fi
 
 echo "Make and install busybox"
-sudo make -j4 CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=arm64 CROSS_COMPILE=${GNU_PATH}/aarch64-none-linux-gnu- install
+sudo make -j4 CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- install
 
 echo "Add library dependencies to rootfs"
 # arm-unknown-linux-gnueabi ??
@@ -146,28 +116,28 @@ cd ${OUTDIR}/rootfs
 echo "======Required for program interpreter======="
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 # [Requesting program interpreter: /lib/ld-linux-aarch64.so.1]
-cp -aL ${LIB_PATH}/aarch64-none-linux-gnu/libc/lib/ld-linux-aarch64.so.1 ${OUTDIR}/rootfs/lib/ld-linux-aarch64.so.1
-#ls -l ${LIB_PATH}/aarch64-none-linux-gnu/libc/lib/ld-linux-aarch64.so.1
+cp -aL ${SYSROOT}/lib/ld-linux-aarch64.so.1 ${OUTDIR}/rootfs/lib/ld-linux-aarch64.so.1
+#ls -l ${SYSROOT}/lib/ld-linux-aarch64.so.1
 # ld-linux-aarch64.so.1 -> ../lib64/ld-2.31.so
-cp -aL ${LIB_PATH}/aarch64-none-linux-gnu/libc/lib64/ld-2.31.so ${OUTDIR}/rootfs/lib/ld-2.31.so
+cp -aL ${SYSROOT}/lib64/ld-2.31.so ${OUTDIR}/rootfs/lib/ld-2.31.so
 
 echo "======Required for Shared library======="
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 # 0x0000000000000001 (NEEDED)             Shared library: [libm.so.6]
 # 0x0000000000000001 (NEEDED)             Shared library: [libresolv.so.2]
 # 0x0000000000000001 (NEEDED)             Shared library: [libc.so.6]
-cp -aL ${LIB_PATH}/aarch64-none-linux-gnu/libc/lib64/libm.so.6 ${OUTDIR}/rootfs/lib64/libm.so.6
-cp -aL ${LIB_PATH}/aarch64-none-linux-gnu/libc/lib64/libresolv.so.2 ${OUTDIR}/rootfs/lib64/libresolv.so.2
-cp -aL ${LIB_PATH}/aarch64-none-linux-gnu/libc/lib64/libc.so.6 ${OUTDIR}/rootfs/lib64/libc.so.6
-#ls -l ${LIB_PATH}/aarch64-none-linux-gnu/libc/lib64/libm.so.6
-#ls -l ${LIB_PATH}/aarch64-none-linux-gnu/libc/lib64/libresolv.so.2
-#ls -l ${LIB_PATH}/aarch64-none-linux-gnu/libc/lib64/libc.so.6
-#~/Programs/install-lnx/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libm.so.6 -> libm-2.31.so
-#~/Programs/install-lnx/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libresolv.so.2 -> libresolv-2.31.so
-#~/Programs/install-lnx/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libc.so.6 -> libc-2.31.so
-cp -aL ${LIB_PATH}/aarch64-none-linux-gnu/libc/lib64/libm-2.31.so ${OUTDIR}/rootfs/lib64/libm-2.31.so
-cp -aL ${LIB_PATH}/aarch64-none-linux-gnu/libc/lib64/libresolv-2.31.so ${OUTDIR}/rootfs/lib64/libresolv-2.31.so
-cp -aL ${LIB_PATH}/aarch64-none-linux-gnu/libc/lib64/libc-2.31.so ${OUTDIR}/rootfs/lib64/libc-2.31.so
+cp -aL ${SYSROOT}/lib64/libm.so.6 ${OUTDIR}/rootfs/lib64/libm.so.6
+cp -aL ${SYSROOT}/lib64/libresolv.so.2 ${OUTDIR}/rootfs/lib64/libresolv.so.2
+cp -aL ${SYSROOT}/lib64/libc.so.6 ${OUTDIR}/rootfs/lib64/libc.so.6
+#ls -l ${SYSROOT}/lib64/libm.so.6
+#ls -l ${SYSROOT}/lib64/libresolv.so.2
+#ls -l ${SYSROOT}/lib64/libc.so.6
+# ${SYSROOT}/lib64/libm.so.6 -> libm-2.31.so
+# ${SYSROOT}/lib64/libresolv.so.2 -> libresolv-2.31.so
+# ${SYSROOT}/lib64/libc.so.6 -> libc-2.31.so
+cp -aL ${SYSROOT}/lib64/libm-2.31.so ${OUTDIR}/rootfs/lib64/libm-2.31.so
+cp -aL ${SYSROOT}/lib64/libresolv-2.31.so ${OUTDIR}/rootfs/lib64/libresolv-2.31.so
+cp -aL ${SYSROOT}/lib64/libc-2.31.so ${OUTDIR}/rootfs/lib64/libc-2.31.so
 
 echo "Make device nodes"
 sudo mknod -m 666 dev/null c 1 3
