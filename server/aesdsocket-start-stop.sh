863b7c9f0cf7134da156596018caab432ddc7356
Vamboozer <marshallfolkman@gmail.com>
2022-10-04 23:10:54 -0600

    Update daemon script

--- server/aesdsocket-start-stop.sh
+++ server/aesdsocket-start-stop.sh
@@ -3,11 +3,12 @@
 case "$1" in
     start)
         echo "Starting aesdsocket"
-        start-stop-daemon -S -n aesdsocket -a /usr/bin/aesdsocket -d
+        #start-stop-daemon --start --name aesdsocket --startas /usr/bin/aesdsocket -- -d
+        start-stop-daemon --start --exec /usr/bin/aesdsocket -- -d
         ;;
     stop)
         echo "Stopping aesdsocket"
-        start-stop-daemon -K -n aesdsocket
+        start-stop-daemon --stop --name aesdsocket
         ;;
     *)
         echo "Usage: $0 {start|stop}"
