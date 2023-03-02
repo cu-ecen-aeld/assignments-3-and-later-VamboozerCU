a7f58c71341236bd2d281d0b8f15b48f6ab7e0c2
Vamboozer <marshallfolkman@gmail.com>
2023-03-01 20:06:39 -0700

    Revert makefile and start-stop script to 6-1

--- server/aesdsocket-start-stop.sh
+++ server/aesdsocket-start-stop.sh
@@ -4,18 +4,15 @@ case "$1" in
     start)
         echo "Starting aesdsocket"
         #start-stop-daemon --start --name aesdsocket --startas /usr/bin/aesdsocket -- -d
-        #start-stop-daemon --start --exec /usr/bin/aesdsocket -- -d
-        /usr/bin/aesdsocket -d
+        start-stop-daemon --start --exec /usr/bin/aesdsocket -- -d
         ;;
     stop)
         echo "Stopping aesdsocket"
-        #start-stop-daemon --stop --name aesdsocket
-        killall /usr/bin/aesdsocket
+        start-stop-daemon --stop --name aesdsocket
         ;;
     *)
         echo "Usage: $0 {start|stop}"
         exit 1
-        ;;
 esac
 
-exit $?
+exit 0
